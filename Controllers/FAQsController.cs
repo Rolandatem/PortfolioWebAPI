b02236e8987bf471b0cfd8e7fa7da11cf53ff009
Steve <steve.martinez@incstores.com>
2025-06-15 00:00:34 -0700

    changed faq order by to descending since we want the most upvoted first.

--- Controllers/FAQsController.cs
+++ Controllers/FAQsController.cs
@@ -21,6 +21,7 @@ public class FAQsController(
 
         return await _context.FAQs
             .Where(faq => faq.IsSiteReady)
+            .OrderByDescending(faq => faq.UpVotes)
             .ToListAsync();
     }
 
@@ -35,7 +36,7 @@ public class FAQsController(
 
         return await _context.FAQs
             .Where(faq => faq.IsSiteReady)
-            .OrderBy(faq => faq.UpVotes)
+            .OrderByDescending(faq => faq.UpVotes)
             .Take(10)
             .ToListAsync();
     }
@@ -46,7 +47,7 @@ public class FAQsController(
     /// <param name="id">DB ID of the FAQ</param>
     /// <returns>FAQ matching the ID.</returns>
     [HttpGet("{id:int}")]
-    public async Task<ActionResult<FAQ>> GetTaskAsync(int id)
+    public async Task<ActionResult<FAQ>> GetFAQAsync(int id)
     {
         await base.DoTestsAsync();
 
@@ -56,5 +57,33 @@ public class FAQsController(
         if (faq == null) { return NotFound(); }
 
         return faq;
+    }
+
+    /// <summary>
+    /// Patches a FAQ by updating the upvotes or downvotes as specified.
+    /// </summary>
+    /// <param name="id">DB ID of the FAQ</param>
+    /// <param name="voteType">"up" or "down"</param>
+    [HttpPatch("{id:int}/vote/{voteType}")]
+    public async Task<IActionResult> VoteAsync(int id, string voteType)
+    {
+        await base.DoTestsAsync();
+
+        if (voteType != "up" && voteType != "down")
+        {
+            return BadRequest($"Expected either '/{id}/vote/up' or '/{id}/vote/down");
+        }
+
+        FAQ? faq = await _context.FAQs
+            .FindAsync(id);
+
+        if (faq == null) { return NotFound(); }
+
+        if (voteType == "up") { faq.UpVotes += 1; }
+        else { faq.DownVotes += 1; }
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
